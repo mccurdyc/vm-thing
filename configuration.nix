44d2d0e72cf21a5b13df2dbde17bf9bfd382e940
Colton J. McCurdy <mccurdyc22@gmail.com>
2023-11-28 07:46:03 -0500

    copy from nixos-anywhere-examples
    Signed-off-by: Colton J. McCurdy <mccurdyc22@gmail.com>

---
+++ configuration.nix
@@ -0,0 +1,31 @@
+# mostly copied from - https://github.com/numtide/nixos-anywhere-examples/blob/a7543e20f630aa3e645d8486d3016327e8f593ed/configuration.nix
+{ modulesPath, config, lib, pkgs, ... }:
+
+{
+  imports = [
+    (modulesPath + "/installer/scan/not-detected.nix")
+    (modulesPath + "/profiles/qemu-guest.nix")
+    ./disk-config.nix
+  ];
+
+  boot.loader.grub = {
+    # no need to set devices, disko will add all devices that have a EF02 partition to the list already
+    # devices = [ ];
+    efiSupport = true;
+    efiInstallAsRemovable = true;
+  };
+
+  services.openssh.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    pkgs.curl
+    pkgs.git
+    pkgs.neovim
+  ];
+
+  users.users.root.openssh.authorizedKeys.keys = [
+    ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO2cxynJf1jRyVzsOjqRYVkffIV2gQwNc4Cq4xMTcsmN
+  ];
+
+  system.stateVersion = "23.05";
+}
--- flake.nix
+++ flake.nix
@@ -1,51 +1,19 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
-    disko.url = "github:nix-community/disko";
-    disko.inputs.nixpkgs.follows = "nixpkgs";
+    disko = {
+      url = "github:nix-community/disko";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = { self, nixpkgs, disko, ... }: {
-
     nixosConfigurations.lvk = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          ({modulesPath, ... }: {
-            imports = [
-              disko.nixosModules.disko
-            ];
-            disko.devices = import ./single-gpt-disk-fullsize-ext4.nix "/dev/sda";
-
-            boot.loader.grub = {
-              copyKernels = true;
-              devices = [ "/dev/sda" ];
-              efiInstallAsRemovable = true;
-              efiSupport = true;
-              enable = true;
-              fsIdentifier = "label";
-            };
-            boot.loader.efi.efiSysMountPoint = "/boot/efi";
-            boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "sd_mod" ];
-
-            networking.hostName = "foo";
-            networking.fqdn = "bar";
-
-            networking.useDHCP = true;
-            networking.interfaces."eth1".useDHCP = true;
-
-            # Initial empty root password for easy login:
-            services.openssh.settings.PermitRootLogin = "prohibit-password";
-
-            users.users.root.openssh.authorizedKeys.keys = [
-            "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO2cxynJf1jRyVzsOjqRYVkffIV2gQwNc4Cq4xMTcsmN"
-            ];
-
-            services.openssh.enable = true;
-
-            system.stateVersion = "23.05";
-          })
-        ];
-      };
-
+      system = "x86_64-linux";
+      modules = [
+        disko.nixosModules.disko
+        ./configuration.nix
+      ];
+    };
   };
 }
